dbbba23fca4ae0d502e8d55e06bdc9216e212d74
Qiang Guo <mcknight0219@gmail.com>
2017-01-27 22:42:16 +0000

    Block interface

--- Sources/Block.swift
+++ Sources/Block.swift
@@ -22,9 +22,39 @@
  // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ///
 
+import Foundation
 
- /// Blocks are separated by new lines.
- public struct Block {
+/// Blocks are separated by new lines.
+public struct Block {
+	/// type of block
+	let kind: Kind
+	/// all inline 
+	var inlines: [Inline]
+	/// configuration for attributed string
+	let config: StyleConfig
+	/// AttributedString applied by styles
+	var attributedString: NSAttributedString {
+		get {
+			let m = NSMutableAttributedString()
+			self.inlines.forEach {inline in
+				m.append(inline.attributedString(self.config))
+			}
+			return m
+		}
+	}
+	/// range of block in text
+	var range: Range<String.Index>
 
- }
+	init(kind: Block.Kind, inlines: [Inline], config: StyleConfig) {
+		self.kind = kind
+		self.inlines = inlines	
+		self.config = config
+	}
 
+	/// Different types of block have different visual representation.
+	enum Kind {
+		case quote
+		case list
+		case paragraph
+	}
+}
